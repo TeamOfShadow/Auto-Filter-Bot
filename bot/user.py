96b0de73ada2f10d012754df9e065c8d228f6168
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-30 16:57:57 +0530

    Auto-Filter-Bot

--- bot/user.py
+++ bot/user.py
@@ -25,4 +25,4 @@ class User(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        self.LOGGER(__name__).info("Bot stopped. Good Bye :)")
