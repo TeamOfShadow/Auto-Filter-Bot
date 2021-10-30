5e5455f171e64a94ede1499778a8b4cbc4a4664b
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-30 17:14:23 +0530

    Auto-Filter-Bot

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,14 +11,11 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
+
 APP_ID = int(os.environ.get("APP_ID"))
-
 API_HASH = os.environ.get("API_HASH")
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
 DB_URI = os.environ.get("DB_URI")
-
 USER_SESSION = os.environ.get("USER_SESSION")
 
 VERIFY = {}
