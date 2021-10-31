ea5004012f10c7a5882cdc80a16bcf179d05cdc3
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-10-31 09:28:29 +0530

    Auto-Filter-Bot

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -59,8 +59,7 @@ async def start(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.first_name),
+        text=Translation.START_TEXT.format(update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
         reply_to_message_id=update.message_id
